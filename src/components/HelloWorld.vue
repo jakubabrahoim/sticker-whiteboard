536f1fbe26cb18eefdfaa4f911c754dcdc950ac9
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-07-14 18:12:20 +0200

    vue setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ message: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+
+    <h1 class='text-red-600 font-bold' >What up</h1>
+</template>
+
