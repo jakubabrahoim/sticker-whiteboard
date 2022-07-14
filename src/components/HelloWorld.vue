ce7e98dfe6026ea05d1ce56e974db56823c5b63e
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-07-14 22:02:50 +0200

    port position

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,10 +13,6 @@
                     { key: "D", color: "lightgreen"},
                 ],
                 linkDataArray: [
-                    { from: "A", to: "B" },
-                    { from: "A", to: "C" },
-                    { from: "B", to: "D" },
-                    { from: "C", to: "D" }
                 ]
             }
         },
@@ -29,21 +25,115 @@
                 "undoManager.isEnabled": true,
             });
             diagram.grid.gridCellSize = new go.Size(20, 20);
+            diagram.grid.opacity = 0.5;
             diagram.toolManager.draggingTool.isGridSnapEnabled = true;
             diagram.toolManager.resizingTool.isGridSnapEnabled = true;
 
             diagram.model = new go.GraphLinksModel(this.nodeDataArray, this.linkDataArray);
 
             // Setup for node (element) and link templates
-            diagram.nodeTemplate = $(go.Node, "Auto", {resizable: true, rotatable: true},
-                $(go.Shape, "Rectangle", {fill: "white"}, new go.Binding("fill", "color"), new go.Binding("stroke", "color")),
-                $(go.TextBlock, "text", {margin: 10}, new go.Binding("text", "key"))
+            diagram.nodeTemplate = $(
+                go.Node, "Spot", 
+                {
+                    resizable: true, 
+                    resizeObjectName: "Sticker", 
+                    rotatable: true,
+                },
+                $(
+                    go.Shape, "Rectangle", 
+                    {
+                        fill: "white",
+                        width: 200, 
+                        height: 200, 
+                        name: "Sticker",
+                    }, 
+                    new go.Binding("fill", "color"), 
+                    new go.Binding("stroke", "color")
+                ),
+                // Left port
+                $(
+                    go.Shape, 'Ellipse',
+                    {
+                        fill: "white",
+                        strokeWidth: 1,
+                        desiredSize: new go.Size(7, 7), alignment: go.Spot.Left,
+                        portId: "left",
+                        fromSpot: go.Spot.Left, toSpot: go.Spot.Left,
+                        fromLinkable: true, toLinkable: true,
+                    }
+                ),
+                // Right port
+                $(
+                    go.Shape, 'Ellipse',
+                    {
+                        fill: "white",
+                        strokeWidth: 1,
+                        desiredSize: new go.Size(7, 7), alignment: go.Spot.Right,
+                        portId: "right",
+                        fromSpot: go.Spot.Right, toSpot: go.Spot.Right,
+                        fromLinkable: true, toLinkable: true,
+                    }
+                ),
+                // Top port
+                $(
+                    go.Shape, 'Ellipse',
+                    {
+                        fill: "white",
+                        strokeWidth: 1,
+                        desiredSize: new go.Size(7, 7), alignment: go.Spot.Top,
+                        portId: "top",
+                        fromSpot: go.Spot.Top, toSpot: go.Spot.Top,
+                        fromLinkable: true, toLinkable: true,
+                    }
+                ),
+                // Bottom port
+                $(
+                    go.Shape, 'Ellipse',
+                    {
+                        fill: "white",
+                        strokeWidth: 1,
+                        desiredSize: new go.Size(7, 7), alignment: go.Spot.Bottom,
+                        portId: "top",
+                        fromSpot: go.Spot.Bottom, toSpot: go.Spot.Bottom,
+                        fromLinkable: true, toLinkable: true,
+                    }
+                ),
+                $(
+                    go.TextBlock, "text", 
+                    {
+                        margin: 10
+                    }, 
+                    new go.Binding("text", "key")
+                )
             );
 
-            diagram.linkTemplate = $(go.Link, {routing: go.Link.AvoidsNodes, corner: 5},
-                $(go.Shape, new go.Binding("stroke", "color")),
-                $(go.Shape, { toArrow: "Standard", stroke: null }, new go.Binding("fill", "color")),
+            diagram.linkTemplate = $(
+                go.Link,
+                {
+                    routing: go.Link.AvoidsNodes, 
+                    corner: 5, 
+                    reshapable: true
+                },
+                $(
+                    go.Shape, 
+                    { strokeWidth: 1.5 }, 
+                    new go.Binding("stroke", "color")
+                ),
+                $(
+                    go.Shape, 
+                    { 
+                        toArrow: "Standard", 
+                        stroke: null 
+                    }, 
+                    new go.Binding("fill", "color")
+                ),
             )
+
+            // These are valid according to the API, ts just buggin
+            // @ts-ignore
+            diagram.model.linkToPortIdProperty = "toPort";
+            // @ts-ignore
+            diagram.model.linkFromPortIdProperty = "fromPort";
         },
         methods: {
             addSticker() {
@@ -73,4 +163,3 @@
         </div>
     </div>
 </template>
-
