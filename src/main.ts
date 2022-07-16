448104787522f38389d4fae835b3700212b95fb2
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-07-16 21:21:52 +0200

    readme update + firebase setup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import './index.css';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import LandingPage from './components/LandingPage.vue';
 import Whiteboard from './components/Whiteboard.vue';
 
@@ -10,6 +13,20 @@ import { OhVueIcon, addIcons } from 'oh-vue-icons';
 import { BiSticky, BiPlusLg, CoMinus, CoSave, BiCloudUpload, BiArrowRightShort, BiEmojiSmile } from 'oh-vue-icons/icons';
 
 addIcons(BiSticky, BiPlusLg, CoMinus, CoSave, BiCloudUpload, BiArrowRightShort, BiEmojiSmile);
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjkFl06qXGER6Q3aYGsz6i5VcmsoI3IRw",
+    authDomain: "whiteboard-sticky-notes.firebaseapp.com",
+    projectId: "whiteboard-sticky-notes",
+    storageBucket: "whiteboard-sticky-notes.appspot.com",
+    messagingSenderId: "393079115439",
+    appId: "1:393079115439:web:c1ca2f94a892ff1a10ce5a",
+    measurementId: "G-K7G7W42K1M"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(firebaseApp);
 
 let routes = [
     { path: '/', component: LandingPage },
